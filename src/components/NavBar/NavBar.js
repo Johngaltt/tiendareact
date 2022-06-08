7aadb1e78d16bb485a135edc8c940aeb6ca8b0c0
Johngaltt <chuampa.ros@gmail.com>
2022-06-07 21:33:03 -0300

    nueva

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export const NavBar = () => {
+
+	return <NavBar><h1>Tienda para Coder</h1>
+    <ul>
+      <li><a href=".">Home</a></li>
+      <li><a href=".">Contacto</a></li>
+      <li><a href=".">Menu</a></li>
+    </ul></NavBar>;
+};
