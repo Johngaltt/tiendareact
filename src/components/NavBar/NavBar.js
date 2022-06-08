670bf0f452953fbdcf62757af3051fa04a0280b9
Johngaltt <chuampa.ros@gmail.com>
2022-06-08 18:33:15 -0300

    Cambios2

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import style from "./NavBar.css"
 
 export const NavBar = () => {
 
-	return <NavBar><h1>Tienda para Coder</h1>
+	return <>
+  <nav className={style.nav}>
+  <h1>Tienda para Coder</h1>
     <ul>
       <li><a href=".">Home</a></li>
       <li><a href=".">Contacto</a></li>
       <li><a href=".">Menu</a></li>
-    </ul></NavBar>;
+    </ul>
+    </nav></>;
 };
